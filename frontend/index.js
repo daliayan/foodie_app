eb61ad24c9899f2acee02998320731d878009405
daliayan <daliayan5@gmail.com>
2021-08-19 20:48:57 -0400

    Styling webpage using css

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,6 @@
 const cityApi = new CityApi('http://localhost:3000/cities')
 const form = document.getElementById('restaurant-form')
+const header = document.getElementById('header')
 
 //loading City data to the document
 document.addEventListener('DOMContentLoaded', () => {
