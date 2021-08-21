8f4698bd87ffad2a94f1f4d0e2a7d250d68e8d6a
daliayan <daliayan5@gmail.com>
2021-08-20 21:16:31 -0400

    New Seed data

--- backend/app/models/restaurant.rb
+++ backend/app/models/restaurant.rb
@@ -1,7 +1,3 @@
 class Restaurant < ApplicationRecord
     belongs_to :city
-    # has_one_attached :avatar
 end
-
-# https://medium.com/@anaharris/how-to-add-image-upload-functionality-to-your-rails-app-9f7fc3f3d042
-#Helpful link to show me how to add images to my restaurant review submission
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,17 +1,11 @@
 nyc = City.create(name: 'New York City')
-boston = City.create(name: 'Boston')
-miami = City.create(name: 'Miami')
-losangeles = City.create(name: 'Los Angeles')
+madrid = City.create(name: 'Madrid')
+rincon = City.create(name: 'Rincón')
+tangier = City.create(name: 'Tangier')
 
 
-Restaurant.create(name: "Kochi", nationality: "Korean", rating: 8, city: nyc)
-Restaurant.create(name: "Gran Gusto", nationality: "Italian", rating: 7, city: boston)
-Restaurant.create(name: "Puerto Sagua", nationality: "Cuban", rating: 9 , city: miami)
-Restaurant.create(name: "Guelaguetza Restaurant", nationality: "Mexican", rating: 7, city: losangeles)
-Restaurant.create(name: "Xelaju", nationality: "Guatemalan", rating: 8, city: nyc)
-
-# avatar: "https://cititour.com/NYC_Restaurants/photos/17475_Kochi%2C%20Korean%2C%20Hell%27s%20Kitchen%2C%20NYC%201.jpg"
-# avatar: "https://tinyurbankitchen.com/wp-content/uploads/2010/06/4708258758_f0d54ebd71_b.jpg" 
-# , avatar: "https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2018/10/26/0/CI_Havana-1957_Miami-best-cuban-restaurants-4.jpg.rend.hgtvcom.1280.853.suffix/1540566302040.jpeg"
-# , avatar: "https://d1ralsognjng37.cloudfront.net/129512af-eb33-4b5b-a4a9-58fa772002a1" 
-# , avatar: "https://duyt4h9nfnj50.cloudfront.net/resized/d8d07154a52cc81df050ca959993cff1-w2880-7f.jpg" 
+Restaurant.create(name: "Misti's", nationality: "Peruvian", rating: 10, city: nyc)
+Restaurant.create(name: "Malvy's Shakes", nationality: "Spanish", rating: 8, city: madrid)
+Restaurant.create(name: "Ricomimi Bakery", nationality: "Puerto Rican", rating: 9 , city: rincon)
+Restaurant.create(name: "Restaurant Ali Baba", nationality: "Moroccan", rating: 9, city: tangier)
+Restaurant.create(name: "Maman", nationality: "French", rating: 8, city: nyc)
