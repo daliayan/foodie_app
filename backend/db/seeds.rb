42715c9fce56d7642e36183cd06685f0ee8ce1f8
daliayan <daliayan5@gmail.com>
2021-08-24 22:20:22 -0400

    Found error - photo in desktop - styling and maybe another method

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -18,13 +18,28 @@ Restaurant.create(name: "Asian 59", nationality: "Chinese", rating: 9, city: nyc
 Restaurant.create(name: "Yummy Gyro", nationality: "Greek", rating: 10, city: nyc)
 Restaurant.create(name: "Maman", nationality: "French", rating: 8, city: nyc)
 Restaurant.create(name: "Chifa Restaurant", nationality: "Peruvian-Japanese", rating: 2, city: nyc)
+
 Restaurant.create(name: "Malvy's Shakes", nationality: "Spanish", rating: 8, city: madrid)
+Restaurant.create(name: "Pizzart Fuencarral", nationality: "Italian", rating: 9, city: madrid)
 Restaurant.create(name: "Chocolatería San Ginés", nationality: "Spanish", rating: 10, city: madrid)
+Restaurant.create(name: "Pituca Madrid", nationality: "Peruvian", rating: 7, city: madrid)
+Restaurant.create(name: "Cherry Pecas", nationality: "Mexican", rating: 7, city: madrid)
+Restaurant.create(name: "The Irish Temple", nationality: "Irish", rating: 10, city: madrid)
+
 Restaurant.create(name: "Ricomimi Bakery", nationality: "Puerto Rican", rating: 9 , city: rincon)
 Restaurant.create(name: "La Taqueria Wepa", nationality: "Puerto Rican", rating: 6 , city: rincon)
 Restaurant.create(name: "Gastropark @ 115", nationality: "Puerto Rican", rating: 9 , city: rincon)
+Restaurant.create(name: "Mangia Mi", nationality: "Italian", rating: 7, city: rincon)
+Restaurant.create(name: "Rincón Smoke Shack", nationality: "American", rating: 9 , city: rincon)
+Restaurant.create(name: "estela Restaurant", nationality: "Vegan-American", rating: 8, city: rincon)
+Restaurant.create(name: "Parrilleria Vacas Gauchas", nationality: "American", rating: 9 , city: rincon)
+
+
 Restaurant.create(name: "Le Loulou", nationality: "French", rating: 9, city: paris)
 Restaurant.create(name: "Le 43 Roof Top", nationality: "French", rating: 7, city: paris)
 Restaurant.create(name: "Ladurée", nationality: "French", rating: 9, city: paris)
 Restaurant.create(name: "Kodawari Ramen", nationality: "Japanese", rating: 9, city: paris)
 Restaurant.create(name: "Presto Fresco", nationality: "Italian", rating: 9, city: paris)
+Restaurant.create(name: "Bistrot Kinzo", nationality: "Japanese", rating: 8, city: paris)
+Restaurant.create(name: "Domenico's", nationality: "Italian", rating: 8, city: paris)
+Restaurant.create(name: "XV Burger", nationality: "American", rating: 9, city: paris)
